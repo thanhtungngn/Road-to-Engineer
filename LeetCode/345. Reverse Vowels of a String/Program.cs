9c682e9a7ad902f92820bfe6ab9c1fb0b1edb2e9
tung.nguyen <thanhtung.ngn@outlook.com>
2024-02-14 11:13:18 +0700

    345

---
+++ LeetCode/345. Reverse Vowels of a String/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine(ReverseVowels("hello"));
+Console.WriteLine(ReverseVowels("leetcode"));
+Console.WriteLine(ReverseVowels("a."));
+
+// BETTER USING STACK + STRING BUILDER 
+
+string ReverseVowels(string s)
+{
+    var result = new StringBuilder();
+    var vowelStack = new Stack<char>();
+
+    foreach (char c in s)
+    {
+        if ("aeiouAEIOU".Contains(c))
+        {
+            vowelStack.Push(c);
+        }
+    }
+
+    foreach (char c in s) {
+        if ("aeiouAEIOU".Contains(c))
+        {
+            result.Append(vowelStack.Pop());
+        }
+        else
+        {
+            result.Append(c);
+        }
+    }
+    return result.ToString();
+}
