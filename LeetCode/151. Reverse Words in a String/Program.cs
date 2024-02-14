284a4171d3eb9b32319912531afd92108eee9dbc
tung.nguyen <thanhtung.ngn@outlook.com>
2024-02-14 11:30:06 +0700

    151

---
+++ LeetCode/151. Reverse Words in a String/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine(ReverseWords("the sky is blue"));
+Console.WriteLine(ReverseWords("  hello world  "));
+Console.WriteLine(ReverseWords("a good   example"));
+
+
+string ReverseWords(string s)
+{
+    var splittedWords = s.Trim().Split(' ');
+
+    var result = new StringBuilder();
+    for (int i = splittedWords.Length-1; i >=0 ; i--)
+    {
+        if (!String.IsNullOrEmpty(splittedWords[i]))
+        {
+            result.Append(splittedWords[i]);
+            result.Append(' ');
+        }
+        
+    }
+    result.Remove(result.Length-1, 1);
+    return result.ToString();
+}
