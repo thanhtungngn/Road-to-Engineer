cc4845d1c89ec0f954e74ab8da8a5c7291a72a10
tung.nguyen <thanhtung.ngn@outlook.com>
2024-02-16 12:25:49 +0700

    2966

---
+++ LeetCode/2966. Divide Array Into Arrays With Max Difference/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Common;
+Console.WriteLine(DivideArray([15, 13, 12, 13, 12, 14, 12, 2, 3, 13, 12, 14, 14, 13, 5, 12, 12, 2, 13, 2, 2], 2).Print());
+//Console.WriteLine(DivideArray([1, 3, 3, 2, 7, 3], 3).Print());
+
+int[][] DivideArray(int[] nums, int k)
+{
+    int[][] result = new int[nums.Length/3][];
+    Array.Sort(nums);
+
+    var index = 0;
+
+    for (int i = 0; i < nums.Length; i = i+3)
+    {
+        if (nums[i+1] - nums[i] <= k 
+            && nums[i+2] - nums[i+1] <= k
+            && nums[i+2] - nums[i] <= k)
+        {
+            result[index] = [nums[i], nums[i + 1], nums[i + 2]];
+            index++;
+        }
+        else
+        {
+            return [];
+        }
+    }
+    return result;
+    
+}
--- LeetCode/Common/TypeExtension.cs
+++ LeetCode/Common/TypeExtension.cs
@@ -15,5 +15,19 @@
             return result;
 
         }
+
+        public static string Print(this int[][] array)
+        {
+            var result = string.Empty;
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] != null )
+                {
+                    result += string.Join(", ", array[i]) + "\n";
+
+                }
+            }
+            return result;
+        }
     }
+
 }
