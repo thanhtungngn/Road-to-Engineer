2dbea2aaa50a94d2928ab6cf1c08b9098487ab5a
tung.nguyen <thanhtung.ngn@outlook.com>
2024-02-20 16:20:44 +0700

    231

---
+++ LeetCode/231. Power of Two/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IsPowerOfTwo(1));
+Console.WriteLine(IsPowerOfTwo(16));
+Console.WriteLine(IsPowerOfTwo(3));
+
+bool IsPowerOfTwo(int n)
+{
+    while (n > 1 && n % 2 == 0)
+    {
+        n /= 2;
+    }
+    return n == 1;
+}
