3589f4a6c69865d49949bbee2b924c6c539f1e96
tung.nguyen <thanhtung.ngn@outlook.com>
2024-03-12 08:23:22 +0700

    791

---
+++ LeetCode/349. Intersection of Two Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Intersection([1, 2, 2, 1], [2, 2]));
+Console.WriteLine(Intersection([4, 9, 5], [9, 4, 9, 8, 4]));
+
+int[] Intersection(int[] nums1, int[] nums2)
+{
+    var result = new List<int>();
+    if(nums1.Length < nums2.Length)
+    {
+        for(int i = 0; i < nums1.Length; i++) {
+            if (nums2.Contains(nums1[i]) && !result.Contains(nums1[i]))
+            {
+                result.Add(nums1[i]);
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < nums2.Length; i++)
+        {
+            if (nums1.Contains(nums2[i]) &&!result.Contains(nums2[i]))
+            {
+                result.Add(nums2[i]);
+            }
+        }
+    }
+    return result.ToArray();
+}
---
+++ LeetCode/791. Custom Sort String/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine(CustomSortString("cba", "abcd"));
+//Console.WriteLine(CustomSortString("bcafg", "abcd"));
+Console.WriteLine(CustomSortString("kqep", "pekeq"));
+
+string CustomSortString(string order, string s)
+{
+    var orderString = new char[s.Length];
+    var index = s.Length - 1;
+    var startIndex = 0;
+    foreach (char c in s)
+    {
+        if (order.IndexOf(c) != -1)
+        {
+            if (startIndex == 0)
+            {
+                orderString[startIndex] = c;
+                startIndex++;
+            }
+            else
+            {
+                if(order.IndexOf(c) > order.IndexOf(orderString[startIndex-1])) {
+                    orderString[startIndex] = c;
+                    startIndex++;
+                }
+                else
+                {
+                    var k = startIndex-1; 
+                    while(k >=0)
+                    {
+                        if(order.IndexOf(c) < order.IndexOf(orderString[k]))
+                        {
+                            orderString[k + 1] = orderString[k];
+                            k--;
+                        }
+                        else
+                        {
+                            orderString[k+1] = c;    
+                            break; 
+                        }
+
+                        if (k == -1)
+                        {
+                            orderString[0] = c;
+                        }
+                    }
+
+         
+                    startIndex++;
+                }
+            }
+        }
+        else
+        {
+            orderString[index] = c;
+            index--;
+        }
+    }
+
+    var result = new StringBuilder();
+    for (int i = 0; i<orderString.Length; i++)
+    {
+        result.Append(orderString[i]);
+    }
+
+    return result.ToString();
+}
