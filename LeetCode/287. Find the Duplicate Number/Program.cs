c7ef1afe12b7b1071da04e19e6e269f4e4b6c2ab
tung.nguyen <thanhtung.ngn@outlook.com>
2024-03-26 01:15:27 +0700

    442

---
+++ LeetCode/287. Find the Duplicate Number/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int FindDuplicate(int[] nums)
+{
+    var result = new int[nums.Length - 1];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (result[nums[i]-1] != 0)
+        {
+            return nums[i];
+        }
+        result[nums[i]-1] = nums[i]; 
+    }
+    return -1;
+}
---
+++ LeetCode/442. Find All Duplicates in an Array/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+IList<int> FindDuplicates(int[] nums)
+{
+    var result = new List<int>();
+    Array.Sort(nums);
+    for (int i = 0; i < nums.Length-1; i++)
+    {
+        if (nums[i + 1] == nums[i])
+        {
+            result.Add(nums[i]);
+            i++;
+        }
+    }
+    return result;
+}
